b52b08a4a1e966c58537ad863651c3ed1d87092f
feud72 <feud72@gmail.com>
2019-09-11 15:20:55 +0900

    studying #8.6 React Events and Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React, {Component} from 'react';
-import Number from "./Number";
+import Number from './Number';
+import {Input, Form} from './Input';
 
 interface IState {
   counter: number;
+  name: string;
 }
 
 class App extends Component<{}, IState> {
   state = {
     counter: 0,
+    name: '',
   };
   render() {
-    const {counter} = this.state;
+    const {counter, name} = this.state;
     return (
       <div>
         <Number count={counter} />
         <button onClick={this.add}>Add</button>
+        <Form onFormSubmit={this.onFormSubmit}>
+          <Input value={name} onChange={this.onChange} />
+        </Form>
       </div>
     );
   }
@@ -24,6 +30,14 @@ class App extends Component<{}, IState> {
         counter: prev.counter + 1,
       };
     });
+  };
+
+  onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    console.log(event.target);
+  };
+
+  onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
   };
 }
 
