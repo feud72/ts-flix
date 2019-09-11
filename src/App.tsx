a83cd9d2db9837d8309145979e2f3306d2cd21a0
feud72 <feud72@gmail.com>
2019-09-11 12:12:11 +0900

    studying ts: #8.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
-};
+interface IState {
+  counter: number;
+}
+
+class App extends Component<{}, IState> {
+  state = {
+    counter: 0,
+  };
+  render() {
+    const {counter} = this.state;
+    return (
+      <div>
+        {counter}
+        <button onClick={this.add}>Add</button>
+      </div>
+    );
+  }
+  add = (): void => {
+    this.setState(prev => {
+      return {
+        counter: prev.counter + 1,
+      };
+    });
+  };
+}
 
 export default App;
