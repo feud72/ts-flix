1e5be2eeca85859ba700d6658236184ade2d6904
feud72 <feud72@gmail.com>
2019-09-16 16:17:54 +0900

    #2.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import {ThemeProvider} from 'styled-components';
-import theme from './theme';
+import App from './Components/App';
 
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
