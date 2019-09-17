eebbabcdceafcfe7e68828c318d27d9005101f72
feud72 <feud72@gmail.com>
2019-09-17 15:55:32 +0900

    #5.5

--- src/Screens/Detail/DetailContainer.tsx
+++ src/Screens/Detail/DetailContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {RouteComponentProps} from 'react-router-dom';
 import DetailPresenter from './DetailPresenter';
 
 interface IState {
@@ -7,21 +8,30 @@ interface IState {
   loading: boolean;
 }
 
-export default class extends React.Component {
-  state: IState = {
+interface IProps extends RouteComponentProps<any> {}
+
+export default class extends React.Component<IProps, IState> {
+  state = {
     result: [],
     error: '',
     loading: true,
   };
 
+  async componentDidMount() {
+    const {
+      match: {
+        params: {id},
+      },
+      history: {push},
+    } = this.props;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push('/');
+    }
+  }
+
   render() {
     const {result, error, loading} = this.state;
-    return (
-      <DetailPresenter
-        result={result}
-        error={error}
-        loading={loading}
-      />
-    );
+    return <DetailPresenter result={result} error={error} loading={loading} />;
   }
 }
