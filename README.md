d7641c086234a848e1ebb047d27b314352c1587d
feud72 <feud72@gmail.com>
2019-09-17 10:48:26 +0900

    #4.3

--- README.md
+++ README.md
@@ -14,3 +14,6 @@
 - [x] Top Rated (TV)
 - [x] Popular (TV, Movie)
 - [x] Airing Today (TV)
+- [x] TV Show Detail
+- [x] Movie Detail
+- [x] Search (Movie, TV)
--- src/Api.tsx
+++ src/Api.tsx
@@ -1,22 +1,45 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-	baseURL: "https://api.themoviedb.org/3/",
-	params: {
-		api_key: "ab9252545142fda52aa83074077e0381",
-		language: "en-US"
-	}
+  baseURL: 'https://api.themoviedb.org/3/',
+  params: {
+    api_key: 'ab9252545142fda52aa83074077e0381',
+    language: 'en-US',
+  },
 });
 
 export const moviesAPI = {
-  nowPlaying: () => api.get("movie/now_playing"),
-	upcoming: () => api.get("movie/upcoming"),
-	popular: () => api.get("movie/popular")
-}
+  nowPlaying: () => api.get('movie/now_playing'),
+  upcoming: () => api.get('movie/upcoming'),
+  popular: () => api.get('movie/popular'),
+  movie: id =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: 'videos',
+      },
+    }),
+  search: term =>
+    api.get('search/movie', {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
+};
 
 export const tvApi = {
-	topRated: () => api.get("tv/top_rated"),
-	popular: () => api.get("tv/popular"),
-	airingToday: () => api.get("tv/airing_today)
-}
-
+  topRated: () => api.get('tv/top_rated'),
+  popular: () => api.get('tv/popular'),
+  airingToday: () => api.get('tv/airing_today'),
+  showDetail: id =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: 'videos',
+      },
+    }),
+  search: term =>
+    api.get('search/tv', {
+      params: {
+        query: encodeURIComponent(term),
+      },
+    }),
+};
