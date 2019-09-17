392ebbf3adc6cc281f1cd69017aa83fdaf4adfbb
feud72 <feud72@gmail.com>
2019-09-17 15:01:51 +0900

    #5.3

--- src/Screens/TV/TVContainer.tsx
+++ src/Screens/TV/TVContainer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TVPresenter from './TVPresenter';
+import {tvApi} from '../../Api';
 
 interface IState {
   topRated: object[];
@@ -17,6 +18,33 @@ export default class extends React.Component {
     loading: true,
     error: '',
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: {results: topRated},
+      } = await tvApi.topRated();
+      const {
+        data: {results: popular},
+      } = await tvApi.popular();
+      const {
+        data: {results: airingToday},
+      } = await tvApi.airingToday();
+      this.setState({
+        topRated,
+        popular,
+        airingToday,
+      });
+    } catch (e) {
+      this.setState({
+        error: e.message,
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
 
   render() {
     const {topRated, popular, airingToday, loading, error} = this.state;
